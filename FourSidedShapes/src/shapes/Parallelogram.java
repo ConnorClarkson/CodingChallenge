aa40f4312f534d8117674f97303b8c3580cb1662
beejorama <brdb92@gmail.com>
2016-11-04 13:00:26 +0000

    Tom helping Craig code parallelogram test - thanks Tom

--- FourSidedShapes/src/shapes/Parallelogram.java
+++ FourSidedShapes/src/shapes/Parallelogram.java
@@ -4,9 +4,20 @@ import point.Point;
 
 public class Parallelogram extends Trapezium{
 
-	public Parallelogram(Point nPoint1, Point nPoint2, Point nPoint3, Point nPoint4) {
+	public Parallelogram(Point nPoint1, Point nPoint2, Point nPoint3, Point nPoint4) throws Exception {
 		super(nPoint1, nPoint2, nPoint3, nPoint4);
-		// TODO Auto-generated constructor stub
+		if (!Parallelogram.isParallelogram(nPoint1, nPoint2, nPoint3, nPoint4)){
+			throw new Exception();
+		}
+		
+	}
+	
+	public static boolean isParallelogram(Point p1, Point p2, Point p3, Point p4){
+		Quad q = new Trapezium(p1, p2, p3, p4);
+		if(q.getSide1().getSlope()==q.getSide3().getSlope() && q.getSide1().getLength()==q.getSide3().getLength()){
+			return true;
+		}
+		return false;
 	}
 
 }
--- FourSidedShapes/src/test/ParallelogramTest.java
+++ FourSidedShapes/src/test/ParallelogramTest.java
@@ -3,13 +3,48 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import point.Point;
+import shapes.Parallelogram;
+
 
 
 public class ParallelogramTest {
 
 	@Test
-	public void testPoint() {
-		
+	public void testParallelogram() {
+		Point p1 = new Point(0, 0);
+		Point p2 = new Point(5, 5);
+		Point p3 = new Point(10, 5);
+		Point p4 = new Point(5, 0);
+		Parallelogram para1;
+		try {
+			para1 = new Parallelogram(p1, p2, p3, p4);
+			assertNotNull("Not created", para1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	public void testIsParallelogram(){
+		Point p1 = new Point(0, 0);
+		Point p2 = new Point(5, 5);
+		Point p3 = new Point(10, 5);
+		Point p4 = new Point(5, 0);
+		boolean b = Parallelogram.isParallelogram(p1, p2, p3, p4);
+		assertTrue("Points don't make a parallelogram", b);
+	}
+	
+	@Test
+	public void testIsNotParallelogram(){
+		Point p1 = new Point(0, 0);
+		Point p2 = new Point(5, 5);
+		Point p3 = new Point(10, 7);
+		Point p4 = new Point(5, 0);
+		boolean b = Parallelogram.isParallelogram(p1, p2, p3, p4);
+		assertFalse("Points do make a parallelogram", b);
 	}
 	
 
