976124c3c87e0e9859bddace2a68cf52464cc585
beejorama <brdb92@gmail.com>
2016-11-04 15:29:46 +0000

    got rid of the errors

--- FourSidedShapes/src/shapes/Square.java
+++ FourSidedShapes/src/shapes/Square.java
@@ -18,13 +18,9 @@ public class Square extends Rectangle {
 
 	public static boolean isSquare(Point p1, Point p2, Point p3, Point p4) {
 		LineClass line1 = new LineClass(p1, p2);
-		System.out.println(line1.getLength());
 		LineClass line2 = new LineClass(p2, p3);
-		System.out.println(line2.getLength());
 		LineClass line3 = new LineClass(p3, p4);
-		System.out.println(line3.getLength());
 		LineClass line4 = new LineClass(p4, p1);
-		System.out.println(line4.getLength());
 		LineClass[] lines = { line1, line2, line3, line4 };
 		if (line1.getLength() == line2.getLength() && line1.getLength() == line3.getLength()
 				&& line1.getLength() == line4.getLength()) {
