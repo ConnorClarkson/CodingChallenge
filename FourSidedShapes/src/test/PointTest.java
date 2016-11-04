100945ebc1d0e1be80f125228da8687960f485f3
beejorama <brdb92@gmail.com>
2016-11-04 12:02:20 +0000

    test fixed to right number of decimals

--- FourSidedShapes/src/test/PointTest.java
+++ FourSidedShapes/src/test/PointTest.java
@@ -40,36 +40,41 @@ public class PointTest {
 		assertTrue("To string not functioning", p.toString().equals("X:2.9, Y:1.5"));
 	}
 	@Test
-	public void testPointDistanceTo1(){
+	public void testPointDistanceToX(){
 		Point p1 = new Point(2.9, 1.9);
 		Point p2 = new Point(1.9, 1.9);
 		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 1);
 	}
 	@Test
-	public void testPointDistanceTo2(){
+	public void testPointDistanceToY(){
 		Point p1 = new Point(2.9, 1.9);
 		Point p2 = new Point(2.9, 0.9);
 		System.out.println(p1.distanceTo(p2));
 		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 1);
 	}
 	@Test
-	public void testPointDistanceTo3(){
+	public void testPointDistanceToTopLeft(){
 		Point p1 = new Point(2, 5);
 		Point p2 = new Point(6,8);
 		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 5);
 	}
 	@Test
-	public void testPointDistanceTo4(){
+	public void testPointDistanceToBottomRight(){
 		Point p1 = new Point(2, 5);
 		Point p2 = new Point(-2, 2);
-		System.out.println(p1.distanceTo(p2));
 		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 5);
 	}
 	@Test
-	public void testPointDistanceTo5(){
+	public void testPointDistanceToUsingNegatives(){
 		Point p1 = new Point(-6, 1);
 		Point p2 = new Point(-2, -2);
 		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 5);
+	}
+	@Test
+	public void testPointDistanceToUsingDoubles(){
+		Point p1 = new Point(2.65, 3.95);
+		Point p2 = new Point(5.1, 1.1);
+		assertTrue("p1 and p2 aren't that distance", p1.distanceTo(p2) == 3.758324094593227);
 	}
 
 }
