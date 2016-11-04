24a4a50c8480687c69b6e9a43d72a4e60cbcd376
beejorama <brdb92@gmail.com>
2016-11-04 14:19:39 +0000

    merge conflicts

--- FourSidedShapes/src/test/ParallelogramTest.java
+++ FourSidedShapes/src/test/ParallelogramTest.java
@@ -47,18 +47,31 @@ public class ParallelogramTest {
 	
 	@Test
 	public void testParallelogramArea(){
-		Point p1 = new Point(0, 0);
-		Point p2 = new Point(5, 5);
-		Point p3 = new Point(10, 5);
-		Point p4 = new Point(5, 0);
+		Point p1 = new Point(5,5);
+		Point p2 = new Point(11, 5);
+		Point p3 = new Point(12, 20);
+		Point p4 = new Point(6, 20);
 		Parallelogram para1;
 		try {
 			para1 = new Parallelogram(p1, p2, p3, p4);
+			assertTrue("Area not 90.0", para1.getArea() == 90.0);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		para1.getArea();
 	}
-	
+	@Test
+	public void testParallelogramPerimeter(){
+		Point p1 = new Point(5,5);
+		Point p2 = new Point(11, 5);
+		Point p3 = new Point(12, 20);
+		Point p4 = new Point(6, 20);
+		Parallelogram para1;
+		try {
+			para1 = new Parallelogram(p1, p2, p3, p4);
+			assertTrue("Area not 42.07", para1.getPerimeter() == 42.06659275674582);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
